bed3261aa890204045468cb38f7e27539f24b683
TanishiSrivastava <42238494+TanishiSrivastava@users.noreply.github.com>
2018-12-01 12:50:53 +0530

    Add files via upload

---
+++ subsetarray.py
@@ -0,0 +1,15 @@
+t1,t2=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+c=0
+for i in l2:
+    if i in l1:
+        c+=1
+    else:
+        c=0
+        break
+if c==0:
+    print("no")
+else:
+    print("yes")
+    
